3c852c3a14e43f280c27fb04051c45b8ffc67e0a
HEX9CF <hex9cf@aliyun.com>
2023-11-02 16:48:22 +0800

    init

---
+++ src/main/java/pawprint/PawprintApplication.java
@@ -0,0 +1,13 @@
+package pawprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawprintApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PawprintApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pawprint/PawprintApplicationTests.java
@@ -0,0 +1,13 @@
+package pawprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawprintApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
